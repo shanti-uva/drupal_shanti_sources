98f5fdba7bd75670283c2ebfda62350a41aa7f22
John De Mott <heydemo13@gmail.com>
2014-08-28 17:42:39 -0500

    Fixing typo

--- sites/all/modules/custom/csc_views/csc_views_breadcrumb_item.tpl.php
+++ sites/all/modules/custom/csc_views/csc_views_breadcrumb_item.tpl.php
@@ -5,8 +5,8 @@
     <?php print $value['term_name_link']; ?>
     <div class="breadcrumb-child-container" style="display: none;">
       <div class="inner-breadcrumb-child-container">
-        <div class="breadcrumb-child-top"><?php print $value['all_srouces_top_link']?></div>
-        <div class="breadcrumb-child-sub-top"><?php print $value['all_srouces_sub_top_link']?></div>
+        <div class="breadcrumb-child-top"><?php print $value['all_sources_top_link']?></div>
+        <div class="breadcrumb-child-sub-top"><?php print $value['all_sources_sub_top_link']?></div>
         <div class="breadcrumb-child"><?php print $value['breadcrumb_term_child']?></div>
       </div>
     </div>
