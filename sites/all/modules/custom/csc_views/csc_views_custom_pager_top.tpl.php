7debd14c0f8d42ce045b79de73be39590420bc66
root <root@opentactics.com>
2015-01-31 23:29:03 -0600

    Implementing top and bottom pager on search result page

--- sites/all/modules/custom/csc_views/csc_views_custom_pager_top.tpl.php
+++ sites/all/modules/custom/csc_views/csc_views_custom_pager_top.tpl.php
@@ -5,7 +5,7 @@
   <div class="custom-footer-pager-right">
     <?php print $value['first_page_link'] . ' ' . $value['prev_page_link']; ?><span>Page</span>
     <input type="text" name="pager-input" id="pager-input" value="<?php print $value['current_page']; ?>"/>
-    of <?php print $value['max_page'] . ' ' . $value['next_page_link'] . ' ' . $value['last_page_link']; ?> 
+    OF <?php print $value['max_page'] . ' ' . $value['next_page_link'] . ' ' . $value['last_page_link']; ?> 
   </div>
   <input type="hidden" value="<?php print $value['max_page']; ?>" id="max-page-input">
 </div>
