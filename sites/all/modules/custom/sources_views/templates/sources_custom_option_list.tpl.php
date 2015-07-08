b063b6965a5c90fba6d909570b01bf948f3aa98e
John De Mott <heydemo13@gmail.com>
2015-07-08 19:08:50 +0200

    Fixing search / autocomplete issue, cleaning up

--- sites/all/modules/custom/sources_views/templates/sources_custom_option_list.tpl.php
+++ sites/all/modules/custom/sources_views/templates/sources_custom_option_list.tpl.php
@@ -1,14 +1,12 @@
 <?php if ($option['key'] == 0): ?>
   <span class="result-item-type <?php print $option['type']; ?>-group-label"><?php print $option['header_label']; ?></span>
 <?php endif; ?>
-<span class="place-published-group">
+<span>
   <?php print $option['value']; ?>
-  <?php if (!empty(print $option['year'])): ?>
+  <?php if (!empty($option['year'])): ?>
     <?php print ' ' . $option['year']; ?>
   <?php endif;?>
-  <?php if (!empty(print $option['author'])): ?>
+  <?php if (!empty($option['author'])): ?>
     <?php print '<br/>' . $option['author']; ?>
   <?php endif;?>
 </span>
-
-
