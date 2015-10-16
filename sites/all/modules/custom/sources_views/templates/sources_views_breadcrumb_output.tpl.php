81578a7366fdb8e1756c58ec3fcf66837f92c1c5
John DeMott <jd7wd@drupal-dev-s.shanti.virginia.edu>
2015-10-16 16:35:30 -0400

    Removing accidentally committed files

--- sites/all/modules/custom/sources_views/templates/sources_views_breadcrumb_output.tpl.php
+++
@@ -1,3 +0,0 @@
-<div class="breadcrumb-item"><?php print $value['home_link']; ?></div>
-<div class="breadcrumb-item"><?php print $value['library_link']; ?></div>
-<?php print $value['breadcrumb_item']; ?>
--- sites/all/modules/taxonomy_block/README.txt
+++
@@ -1,1 +0,0 @@
-Drupal 7 version of the taxonomy_block module (http://drupal.org/project/taxonomy_block).
